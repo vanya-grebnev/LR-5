fa9a4ab2e50399ab647c313156bb4a69dde52429
sasha <stalker.skk@gmail.com>
2023-05-30 10:55:38 +0300

    оставшиеся строки кода

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print("Grebnev PI-214")
 array = [10,5,30,50,40]
 array2 = []
+for i in range(0,len(array)):
+    if array[i] == max(array) or array[i] == min(array):
+        array2.append(array[i] * 3)
+    else:
+        array2.append(array[i] * 2)
+print(array2)
